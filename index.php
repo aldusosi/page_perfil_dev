c7e9e189bd0e662004e8741f98490a878d7f1598
aldusosi <desenho2510@gmail.com>
2024-06-16 23:55:39 -0300

    adicionado sections Sobre, Tools e Beneficios

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <title>Document</title>
     </head>
     <body>
+
         <header>
             <div class="center">
                 <div class="logotipo"><h1><a href="">Alexsandro D. Silva</a></h1></div>
@@ -28,8 +29,9 @@
                 </nav>
             </div>
         </header><!--Header-->
-        <section class="apresentacao">
-            <div class="bem-vindo">
+
+        <section class="apresentacao center">
+            <div class="bem-vindo w50">
                 <div>
                     <p>Front-end | Back-end</p>
                     <h1>Ola, sou o Alex!</h1>
@@ -37,8 +39,109 @@
                     <a class="buttom-contato" href="">Contato</a>
                 </div>
             </div>
-            <div class="image-apresentacao">
+            <div class="image-apresentacao w50">
                 <img src="./images/image-apresentacao.png" alt="imagen-apresentacao">
+            </div>
+        </section><!--Apresentacao-->
+
+        <section class="sobre center">
+            <h2>Sobre Mim</h2>
+            <div class="sobre-text w50">
+                <p>Sou Alexsandro Duarte. Me formei em Jogos Digitais e sempre fui apaixonado por tecnologia, busco estar sempre atualizado tanto as questões relacionadas ao desenvovimento web quanto aos jogos digitais. Acredito que essa busca acaba facilitando no entendimento de novos conteúdos pois me leva a locais amplos de aprendizado. Qualidade no que faço é uma busca constante pois é ela quem indica o meu grau de conhecimento.</p>
+            </div>
+            <div class="sobre-foto w50">
+                <img src="./images/Alex.jpg" alt="">
+            </div>
+        </section><!--Sobre-->
+
+        <section class="tools">
+            <div><h2>Tools</h2></div>
+            <div class="center">
+                <div class="front-end-tools">
+                    <div><h3>Front-end</h3></div>
+                    <div class="tools-container">
+                        <div class="tool-card">
+                            <div class="tool-image"></div>
+                            <p>Html</p>
+                        </div>
+                        <div class="tool-card">
+                            <div class="tool-image"></div>
+                            <p>CSS</p>
+                        </div>
+                        <div class="tool-card">
+                            <div class="tool-image"></div>
+                            <p>JS</p>
+                        </div>
+                        <div class="tool-card">
+                            <div class="tool-image"></div>
+                            <p>Bootstrap</p>
+                        </div>
+                        <div class="tool-card">
+                            <div class="tool-image"></div>
+                            <p>React</p>
+                        </div>
+                    </div>
+                </div><!--Front-end Tools-->
+
+                <div class="back-end-tools">
+                    <div><h3>Back-end</h3></div>
+                    <div class="tools-container">
+                        <div class="tool-card">
+                            <div class="tool-image"></div>
+                            <p>PHP</p>
+                        </div>
+                        <div class="tool-card">
+                            <div class="tool-image"></div>
+                            <p>MySql</p>
+                        </div>
+                        <div class="tool-card">
+                            <div class="tool-image"></div>
+                            <p>Node</p>
+                        </div>
+                    </div>
+                </div><!--Front-end Tools-->
+            </div>
+        </section><!--Tools-->
+
+        <section class="seus-beneficios center">
+            <h2>Seus Benefícios</h2>
+            <div class="beneficios-container">
+                <div class="beneficio-card">
+                    <div class="beneficio-circle">
+                        <div class="image-icon"></div>
+                    </div>
+                    <p>Mais tempo pra focar no seu projeto</p>
+                </div>
+                <div class="beneficio-card">
+                    <div class="beneficio-circle">
+                        <div class="image-icon"></div>
+                    </div>
+                    <p>Atraia clientes com algo de qualidade</p>
+                </div>
+                <div class="beneficio-card">
+                    <div class="beneficio-circle">
+                        <div class="image-icon"></div>
+                    </div>
+                    <p>Mostre suas idéias de forma organizada</p>
+                </div>
+                <div class="beneficio-card">
+                    <div class="beneficio-circle">
+                        <div class="image-icon"></div>
+                    </div>
+                    <p>Aumente seu público com o alcance da internet</p>
+                </div>
+                <div class="beneficio-card">
+                    <div class="beneficio-circle">
+                        <div class="image-icon"></div>
+                    </div>
+                    <p>Seu lindo web site trabalhando 24hrs por dia</p>
+                </div>
+                <div class="beneficio-card">
+                    <div class="beneficio-circle">
+                        <div class="image-icon"></div>
+                    </div>
+                    <p>Você no controle de forma simples</p>
+                </div>
             </div>
         </section>
     </body>
