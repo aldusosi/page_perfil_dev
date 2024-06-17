dc9b34c193522231fed32e3229796d2708a4025e
aldusosi <desenho2510@gmail.com>
2024-06-16 23:24:12 -0300

    html sections header e apresentacao

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Precisa de uma página web? confira o trabalho do desenvolvedor Alexsandro Duarte, dev front-and, back-end, com páginas profissionais que atendem as suas demandas">
+        <title>Document</title>
+    </head>
+    <body>
+        <header>
+            <div class="center">
+                <div class="logotipo"><h1><a href="">Alexsandro D. Silva</a></h1></div>
+                <nav class="nav-desktop">
+                    <ul>
+                        <li><a href="">Projetos</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Contatos</a></li>
+                        <li><a href="">Serviços</a></li>
+                    </ul>
+                </nav>
+                <nav class="nav-mobile">
+                    <ul>
+                        <li><a href="">Projetos</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Contatos</a></li>
+                        <li><a href="">Serviços</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header><!--Header-->
+        <section class="apresentacao">
+            <div class="bem-vindo">
+                <div>
+                    <p>Front-end | Back-end</p>
+                    <h1>Ola, sou o Alex!</h1>
+                    <p>Bem vindo ao meu espaço tecnológico. Esta página é dedicada ao meu trabalho e experiências profissionais</p>
+                    <a class="buttom-contato" href="">Contato</a>
+                </div>
+            </div>
+            <div class="image-apresentacao">
+                <img src="./images/image-apresentacao.png" alt="imagen-apresentacao">
+            </div>
+        </section>
+    </body>
+</html>
